293bd42143ecd7ac87a50516c494803c8ccda417
Shashikant364 <79153181+Shashikant364@users.noreply.github.com>
2021-02-17 17:58:29 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,27 @@
-echo "Welcome to Guessing the number game!!!"
-echo "Instructions: You need to guess how many files are in the current directory until you guess the correct answer."
- 
-function ask {
-  echo "How many files are there in the current directory?"
-  read guess 
-## The Command ls-l | wc -l gives one more than the number of files.
-  number_of_files=$(ls -1 | wc -l)
-}
-ask
-while [[ $guess -ne $number_of_files ]]
-do 
-  if [[ $guess -ge $number_of_files ]] 
-  then
-    echo "Try a smaller number than the specified one."
-  else [[ $guess -le $number_of_files ]]
-    echo "Try a bigger number than the specified one." 
-  fi
-  ask
-done
-echo "Congratulation you guessed the right number"
-echo "There are $number_of_files files in the current directory, which are: "
-echo "--------------" && ls
+#!/usr/bin/env/ bash
+# File: guessinggame.sh
 
+correct_number=$(ls | wc -l)
+
+function guess {
+	read guess
+	if [[ $guess -eq $correct_number ]]
+	then
+		echo "Congratulations!!, You guessed it right."
+                echo " We have "
+                for f in $(ls)
+                do
+                        echo " - $f "
+                done
+	elif [[ $guess -lt $correct_number ]]
+		then
+		echo "Your guess is too low, please try again"
+		guess
+	else
+		echo "Your guess is too high, please try again"
+		guess
+	fi
+}
+echo "Welcome to the guessing game!"
+echo "How many files are there in the direcoty?"
+guess
